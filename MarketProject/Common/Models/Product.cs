4037fff37f7cc0204935569f1844e158bbe22691
NamiqMy <namiqmy@code.edu.az>
2023-07-12 00:00:15 +0400

    Creating new Classes

---
+++ MarketProject/Common/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using MarketProject.Common.Base;
+using MarketProject.Common.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketProject.Common.Models
+{
+    public class Product : BaseEntity
+    {
+        private static int _count = 0;
+        public Product() 
+        {
+            Id = _count;
+            _count++;
+        }
+
+        public string Pr_Name { get; set; }
+        public double Pr_Price { get; set;}
+        public int Pr_Count { get; set; }
+        public int Pr_Code { get; set; }
+        public Category Category { get; set; }
+    }
+}

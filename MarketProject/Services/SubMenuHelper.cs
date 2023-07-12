9d5a067040628e4f60bd0e3a653aa736320a6524
NamiqMy <namiqmy@code.edu.az>
2023-07-12 23:49:55 +0400

    Added SubMenuHelper class to my project

---
+++ MarketProject/Services/SubMenuHelper.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketProject
+{
+    public static class SubMenuHelper
+    {
+        public static void ProductSubMenu()
+        {
+            Console.Clear();
+            int option;
+            do
+            {
+                Console.WriteLine("1. Add new product");
+                Console.WriteLine("2. Edit the product");
+                Console.WriteLine("3. Delete the product");
+                Console.WriteLine("4. Show all products");
+                Console.WriteLine("5. Show products by category");
+                Console.WriteLine("6. Show products by price range");
+                Console.WriteLine("7. Search products by name");
+                Console.WriteLine("0. Exit");
+
+                Console.WriteLine("-----------");
+                Console.WriteLine("Enter option:");
+
+                while (!int.TryParse(Console.ReadLine(), out option))
+                {
+                    Console.WriteLine("Invalid number!");
+                    Console.WriteLine("-----------");
+                    Console.WriteLine("Enter option:");
+                }
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Added new product");
+                        break;
+                    case 2:
+                        Console.WriteLine("Edited the product");
+                        break;
+                    case 3:
+                        Console.WriteLine("Deleted the product");
+                        break;
+                    case 4:
+                        Console.WriteLine("Shown all products");
+                        break;
+                    case 5:
+                        Console.WriteLine("Shown products by category");
+                        break;
+                    case 6:
+                        Console.WriteLine("Shown products by price range");
+                        break;
+                    case 7:
+                        Console.WriteLine("Searched products by name");
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        Console.WriteLine("There is no such option!");
+                        break;
+                }
+
+            } while (option != 0);
+
+        }
+
+        public static void SaleSubMenu()
+        {
+            Console.Clear();
+            int option;
+            do
+            {
+                Console.WriteLine("1. Add new sale");
+                Console.WriteLine("2. Return of product");
+                Console.WriteLine("3. Deletion of sale");
+                Console.WriteLine("4. Display of all sales");
+                Console.WriteLine("5. Display of sales for a given date range");
+                Console.WriteLine("6. Display sales by given amount range");
+                Console.WriteLine("7. Showing sales for a given date");
+                Console.WriteLine("8. Displaying sales information based on a given number");
+                Console.WriteLine("0. Go back");
+
+                Console.WriteLine("-----------");
+                Console.WriteLine("Enter option:");
+
+                while (!int.TryParse(Console.ReadLine(), out option))
+                {
+                    Console.WriteLine("Invalid number!");
+                    Console.WriteLine("-----------");
+                    Console.WriteLine("Enter option:");
+                }
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Added neü sale");
+                        break;
+                    case 2:
+                        Console.WriteLine("The product was returned");
+                        break;
+                    case 3:
+                        Console.WriteLine("The product has been deleted");
+                        break;
+                    case 4:
+                        Console.WriteLine("Displayed all sales");
+                        break;
+                        case 5:
+                        Console.WriteLine("Displayed of sales for a given date range");
+                        break;
+                        case 6:
+                        Console.WriteLine("Displayed sales by given amount range");
+                        break;
+                        case 7:
+                        Console.WriteLine("Showed sales for a given date");
+                        break;
+                        case 8:
+                        Console.WriteLine("Displayed sales information based on a given number");
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        Console.WriteLine("There is no such option!");
+                        break;
+                }
+
+            } while (option != 0);
+        }
+    }
+}
+    
+
